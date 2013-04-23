c490472a96be3e3c6c7d1b146a74ad0ab58bab35
Mark Chavez <los_populares@yahoo.com>
2013-04-23 23:09:18 +0800

    Added new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130423141859) do
+ActiveRecord::Schema.define(:version => 20130423145551) do
 
   create_table "avatars", :force => true do |t|
     t.string   "photo_file_name"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(:version => 20130423141859) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
